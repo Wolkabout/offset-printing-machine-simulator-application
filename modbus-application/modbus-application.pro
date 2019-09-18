30489badad64577b022ea2bc881786b1122bcf2d
nexylmao <nenadvvvvvvv@gmail.com>
2019-09-18 13:54:35 +0200

    Fixed not added components

--- modbus-application/modbus-application.pro
+++ modbus-application/modbus-application.pro
@@ -12,8 +12,8 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 TARGET = modbus-application
 TEMPLATE = app
 
-QMAKE_CXXFLAGS += -std=gnu++0x -pthread
-QMAKE_CFLAGS += -std=gnu++0x -pthread
+QMAKE_CXXFLAGS += -std=gnu++17 -pthread
+QMAKE_CFLAGS += -std=gnu++17 -pthread
 
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which has been marked as deprecated (the exact warnings
@@ -26,7 +26,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-CONFIG += c++17
+CONFIG += c++17 thread
 UI_DIR = $$PWD
 
 SOURCES += \
--- modbus-application/simulator.cpp
+++ modbus-application/simulator.cpp
@@ -4,12 +4,19 @@ Simulator::Simulator()
 {
     machine = std::make_shared<Machine>("Main Machine");
     feeder = std::make_shared<Feeder>("Feeder", *machine.get(), FEEDER_CAPACITY, FEEDER_START_COUNT);
+    machine->addComponent(feeder);
     cyan = std::make_shared<PaintStation>("Cyan Paint", *machine.get(), PAINT_STATION_CAPACITY, PAINT_STATION_START_COUNT);
+    machine->addComponent(cyan);
     magenta = std::make_shared<PaintStation>("Magenta Paint", *machine.get(), PAINT_STATION_CAPACITY, PAINT_STATION_START_COUNT);
+    machine->addComponent(magenta);
     yellow = std::make_shared<PaintStation>("Yellow Paint", *machine.get(), PAINT_STATION_CAPACITY, PAINT_STATION_START_COUNT);
+    machine->addComponent(yellow);
     black = std::make_shared<PaintStation>("Black Paint", *machine.get(), PAINT_STATION_CAPACITY, PAINT_STATION_START_COUNT);
+    machine->addComponent(black);
     delivery = std::make_shared<Delivery>("Delivery", *machine.get(), DELIVERY_CAPACITY, DELIVERY_START_COUNT);
+    machine->addComponent(delivery);
     conveyor = std::make_shared<Conveyor>("Conveyor Belt", *machine.get(), CONVEYOR_MAX_RATE, CONVEYOR_START_RATE);
+    machine->addComponent(conveyor);
 }
 
 shared_ptr<Machine> Simulator::getMachine()
