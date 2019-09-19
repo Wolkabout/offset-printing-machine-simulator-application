5521f4419f035f51152a1f18afdbf9b305274e83
nexylmao <nenadvvvvvvv@gmail.com>
2019-09-19 16:48:57 +0200

    Got conveyor controls working

--- modbus-application/conveyorcontrol.cpp
+++ modbus-application/conveyorcontrol.cpp
@@ -7,9 +7,47 @@ ConveyorControl::ConveyorControl(Conveyor& conveyor, QWidget *parent) :
     ui(new Ui::ConveyorControl)
 {
     ui->setupUi(this);
+    ui->name->setText("<h2> " + QString::fromStdString(conveyor.getName()) + "</h2>");
+
+    listener = std::make_shared<RateListener>(*this, conveyor, ui->rate);
+    conveyor.getRateMessageReceivers().push_back(listener);
+
+    ui->rate->setText("<h2>" + QString::number(conveyor.getRatePerHour()) + "</h2>");
 }
+
+ConveyorControl::RateListener::RateListener(ConveyorControl& cc, Conveyor& conveyor, QLabel * rateLabel)
+    : cc(cc), conveyor(conveyor), rateLabel(rateLabel) { }
+
+void ConveyorControl::RateListener::ReceiveMessage(std::shared_ptr<ConveyorRateMessage> message) {
+    QMetaObject::invokeMethod(rateLabel, "setText", Qt::QueuedConnection, Q_ARG(QString, "<h2>" + QString::number(message->getCurrentRate()) + "</h2>"));
+};
 
 ConveyorControl::~ConveyorControl()
 {
     delete ui;
+}
+
+void ConveyorControl::on_decrease_clicked()
+{
+    if (conveyor.getRatePerHour() > conveyor.getMinRatePerHour()) {
+        conveyor.setRatePerHour(conveyor.getRatePerHour() - 100);
+    }
+}
+
+void ConveyorControl::on_increase_clicked()
+{
+    if (conveyor.getRatePerHour() < conveyor.getMaxRatePerHour()) {
+        conveyor.setRatePerHour(conveyor.getRatePerHour() + 100);
+    }
+}
+
+void ConveyorControl::on_failure_clicked()
+{
+    hide();
+    conveyor.Emit(Severe, conveyor.getName() + " has stopped working!");
+}
+
+void ConveyorControl::on_ok_clicked()
+{
+    hide();
 }
--- modbus-application/conveyorcontrol.h
+++ modbus-application/conveyorcontrol.h
@@ -1,6 +1,7 @@
 #ifndef CONVEYORCONTROL_H
 #define CONVEYORCONTROL_H
 
+#include <QLabel>
 #include <QWidget>
 
 #include <Components/Conveyor.h>
@@ -17,9 +18,29 @@ public:
     ConveyorControl(Conveyor& conveyor, QWidget *parent = nullptr);
     ~ConveyorControl();
 
+private slots:
+    void on_decrease_clicked();
+
+    void on_increase_clicked();
+
+    void on_failure_clicked();
+
+    void on_ok_clicked();
+
 private:
     Ui::ConveyorControl *ui;
     Conveyor& conveyor;
+    std::shared_ptr<ConveyorRateMessageReceiver> listener;
+
+    class RateListener : public ConveyorRateMessageReceiver {
+    private:
+        ConveyorControl& cc;
+        Conveyor& conveyor;
+        QLabel * rateLabel;
+    public:
+        RateListener(ConveyorControl&, Conveyor&, QLabel *);
+        void ReceiveMessage(std::shared_ptr<ConveyorRateMessage>) override;
+    };
 };
 
 #endif // CONVEYORCONTROL_H
