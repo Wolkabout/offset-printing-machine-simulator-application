290e699728aac956b43d00c465371c87ac32a78a
nexylmao <nenadvvvvvvv@gmail.com>
2019-11-19 16:14:50 +0100

    Write to bits, not registers

--- modbus-application/bittogglethread.h
+++ modbus-application/bittogglethread.h
@@ -13,11 +13,14 @@ public:
     BitToggleThread(int bit, modbus_mapping_t * mappings) {
         this->bit = bit;
         this->mappings = mappings;
+        qDebug("Created thread for bit %i", bit);
     }
     void run() override {
-        mappings->tab_input_registers[bit] = 1;
+//        qDebug("Starting thread for bit %i (prev. value %i, setting to %i)", bit, mappings->tab_input_registers[bit], 1);
+        mappings->tab_input_bits[bit] = 1;
         std::this_thread::sleep_for(std::chrono::seconds(5));
-        mappings->tab_input_registers[bit] = 0;
+//        qDebug("Stopping thread for bit %i (prev. value %i, setting to %i)", bit, mappings->tab_input_registers[bit], 0);
+        mappings->tab_input_bits[bit] = 0;
         finished();
     }
 };
