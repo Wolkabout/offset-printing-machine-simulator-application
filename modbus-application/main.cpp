e7730a2eccbda5ce63d70391690b27d48fe8734a
nvuletic <nenad.vuletic@wolkabout.com>
2019-12-17 17:58:40 +0100

    Extended wait

--- modbus-application/main.cpp
+++ modbus-application/main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 
         int res = a.exec();
         modbusThread.terminate();
-        modbusThread.wait(2);
+        modbusThread.wait(5);
         return res;
     } catch (std::exception &e) {
         qDebug("%s", e.what());
